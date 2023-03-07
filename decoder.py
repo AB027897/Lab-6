2c9ba5bcfce7d43b53322eea77ff047d70563925
AB027897 <47727880+AB027897@users.noreply.github.com>
2023-03-07 14:31:13 -0500

    Pushed decoder.py

---
+++ decoder.py
@@ -0,0 +1,5 @@
+def decoder(password):
+    newPassword = ""
+    for i in password:
+        newPassword += (int(i) - 3)
+    return newPassword
---
+++ encoder.py
@@ -0,0 +1,13 @@
+def encoder(password):
+    newPassword = ""
+    for i in password:
+        newPassword += (int(i)+ 3)
+    return newPassword
+
+
+
+
+
+
+
+
