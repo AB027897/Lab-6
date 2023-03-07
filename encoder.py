6bcd5aea2ef24affe4cf30bd5057a9dee98b2156
AB027897 <47727880+AB027897@users.noreply.github.com>
2023-03-07 15:07:59 -0500

    fixed encoder

---
+++ encoder.py
@@ -0,0 +1,33 @@
+# Adam Benali
+from decoder import decode
+def encoder(password):
+    newPassword = ""
+    for i in password:
+        newPassword += str(int(i)+ 3 % 10)
+    return newPassword
+
+if __name__ == "__main__":
+    password = ""
+    running = True
+    while running:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        user_input = int(input("Please enter an option:"))
+        if user_input == 1:
+            password = input("Please enter your password to encode:")
+            print("Your password has been encoded and stored!")
+        elif user_input == 2:
+            decoded = decode(password)
+            print(f"The encoded password is {decoded}, and the original password is {password}")
+        else:
+            running = False
+
+
+
+
+
+
+
